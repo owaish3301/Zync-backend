9cbd54d18a3d33ff007f33fc1ff42c6af14e3d37
Owaish <owaish3301@gmail.com>
2026-04-30 11:24:01 +0530

    project started

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
