344126b0d8a676830107444294fe6f204646234b
Owaish <owaish3301@gmail.com>
2026-04-30 12:15:19 +0530

    added prisma

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,14 @@
 import "dotenv/config";
 
-const PORT = process.env.PORT;
+function validateAndGetEnv(name: string): string {
+  const env = process.env[name];
+  if (!env) {
+    throw new Error(`Configuration Error for ${name}`);
+  }
+  return env;
+}
 
-export { PORT };
+const PORT = validateAndGetEnv("PORT");
+const DATABASE_URL = validateAndGetEnv("DATABASE_URL");
+
+export { PORT, DATABASE_URL };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { PORT } from "./config/env.js";
 
 const app = express();
 
+app.get("/api/health", (req, res, next) => {
+  res.status(200).json({ message: "All good!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
