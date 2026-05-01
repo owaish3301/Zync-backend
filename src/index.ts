870eff46e8284fc6d88f48e6ddea4fbac160391d
Owaish <owaish3301@gmail.com>
2026-05-01 12:11:58 +0530

    add better auth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
+
+app.use("/api/auth/**", toNodeHandler(auth));
+
+app.use(express.json());
 
 app.get("/api/health", (req, res, next) => {
   res.status(200).json({ message: "All good!" });
---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { DATABASE_URL } from "../config/env";
+import { username } from "better-auth/plugins";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: DATABASE_URL,
+  }),
+});
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins:[
+    username()
+  ],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
