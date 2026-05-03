b2a811f755fc6335f5ea499baedc276c51505280
Owaish <owaish3301@gmail.com>
2026-05-03 16:01:15 +0530

    typo fix

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -9,7 +9,7 @@ const router = Router();
 router.get("/:username", getUser); // this route returns the user data to show a public profile, this dont need auth
 router.get("/:id/approve", validateAuth, requireActive, checkSuperAdmin, activateUser) // superadmin can upgrade a pending user to active
 router.get("/:id/ban", validateAuth, requireActive, checkSuperAdmin, banUser) // superadmin can ban a user
-router.delete("", validateAuth, requireActive, checkSuperAdmin, deleteUser) // superadmin can delete a user
+router.delete("/:id", validateAuth, requireActive, checkSuperAdmin, deleteUser) // superadmin can delete a user
 
 
 export { router as userRouter };
