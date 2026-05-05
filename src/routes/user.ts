8594d8597e036a551f19b845457276a79f39f640
Owaish <owaish3301@gmail.com>
2026-05-05 20:30:36 +0530

    refactor user routes to use PATCH method for approve and ban actions

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,10 +6,10 @@ import requireActive from "../middlewares/requireActive";
 
 const router = Router();
 
-router.get("/:username", getUser); // this route returns the user data to show a public profile, this dont need auth
-router.get("/:id/approve", validateAuth, requireActive, checkSuperAdmin, activateUser) // superadmin can upgrade a pending user to active
-router.get("/:id/ban", validateAuth, requireActive, checkSuperAdmin, banUser) // superadmin can ban a user
+router.patch("/:id/approve", validateAuth, requireActive, checkSuperAdmin, activateUser) // superadmin can upgrade a pending user to active
+router.patch("/:id/ban", validateAuth, requireActive, checkSuperAdmin, banUser) // superadmin can ban a user
 router.delete("/:id", validateAuth, requireActive, checkSuperAdmin, deleteUser) // superadmin can delete a user
+router.get("/:username", getUser); // this route returns the user data to show a public profile, this dont need auth
 
 
 export { router as userRouter };
